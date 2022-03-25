5519ba80f4084218d288e97c656ffe81ae81a504
Amir Mousavi <38921349+aamirmousavi@users.noreply.github.com>
2022-03-25 19:34:09 +0430

    Update getid.go

--- getid.go
+++ getid.go
@@ -2,19 +2,6 @@ package htmlreader
 
 import "golang.org/x/net/html"
 
-func (n *Reader) traverseId(id string) (*html.Node, bool) {
-	if n.check("id", id) {
-		return n.HTML, true
-	}
-	for c := n.HTML.FirstChild; c != nil; c = c.NextSibling {
-		res, ok := ConvReader(c).traverseId(id)
-		if ok {
-			return res, true
-		}
-	}
-	return nil, false
-}
-
 func (n *Reader) GetElementById(id string) (*html.Node, bool) {
-	return n.traverseClass(id)
+	return n.traverse("id", id)
 }
