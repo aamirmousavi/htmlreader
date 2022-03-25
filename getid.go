4ed718ce1bb36672524e7e562700493b1883765c
Amir Mousavi <38921349+aamirmousavi@users.noreply.github.com>
2022-03-25 19:21:28 +0430

    Update getid.go

--- getid.go
+++ getid.go
@@ -7,7 +7,7 @@ func (n *Reader) traverseId(id string) (*html.Node, bool) {
 		return n.HTML, true
 	}
 	for c := n.HTML.FirstChild; c != nil; c = c.NextSibling {
-		res, ok := convReader(c).traverseId(id)
+		res, ok := ConvReader(c).traverseId(id)
 		if ok {
 			return res, true
 		}
